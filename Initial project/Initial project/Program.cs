113140c2602e93cb2c80959bcabffb10cc39f7f1
Jakub <kubakarwow@gmail.com>
2024-03-18 20:31:44 +0100

    Implemented Max Function

--- Initial project/Initial project/Program.cs
+++ Initial project/Initial project/Program.cs
@@ -5,6 +5,19 @@ Console.WriteLine("mod 1");
 Console.WriteLine("mod2");
 Console.WriteLine("mod3");
 
+static int Max(int[] tab)
+{
+    int result = 0;
+    for (int i = 0; i < tab.Length; i++)
+    {
+        if (result < tab[i])
+        {
+            result = tab[i];
+        }
+    }
+
+    return result;
+}
 static double average(int[] tab)
 {
     double result = 0;
